11aecb0019b0e00b733ef23b578a903ca9ba4dd5
Julien <julieniancu@gmail.com>
2021-02-19 00:37:50 +0200

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,92 @@
+<script>
+  import Login  from "./Login.svelte";
+  let is_logged = false;
+  function show_register() {
+	is_registered=true;
+	show_login();
+  }
+  function show_login() {
+    is_logged = true;
+  }
+  function hide_login() {
+    is_logged = false;
+	is_registered= false;
+  }
+  let is_registered= false;
+</script>
+
+<main>
+  <div class="box" />
+  <div class="top_line" />
+  <h1>Referatele grozave</h1>
+  
+  <button class="register_btn"  on:click={show_register}>Register</button>
+  <button class="login_btn" on:click={show_login}>Login</button>
+  {#if is_logged}
+    <Login {is_registered}  on:closeIt={hide_login} />		
+  {/if}
+  <img class="Carte" src="../Poze/Carti.png" alt="Cardati">
+</main>
+
+<style>
+  .Carte{
+	position: absolute;
+    top: 50px;
+	left: 10%;
+	height: 140px;
+	z-index: 1;
+	
+  }
+  h1 {
+    position: absolute;
+    text-align: center;
+	left: 43%;
+	top:70px;
+    font-style: italic;
+    height: 30vw;
+	z-index: 1;
+    
+  }
+  .login_btn {
+    position: absolute;
+    padding: 0px;
+    border: solid 3px rgb(0, 0, 0);
+    left: 92%;
+    top: 0vw;
+	font-style: italic;
+    height: 40px;
+    width: 80px;
+    z-index: 2;
+  }
+  .register_btn {
+    position: absolute;
+    padding: 0px;
+    border: solid 3px rgb(0, 0, 0);
+    left: 85%;
+	font-style:italic ;
+    top: 0px;
+    height: 40px;
+    width: 80px;
+    z-index: 2;
+  }
+  .box {
+    position: fixed;
+    top: 0%;
+    left: 0%;
+	background-image: url("../Poze/Fundal.jpg");
+    border-bottom: 6px solid rgb(3, 3, 3);
+    width: 110vw;
+    height: 200px;
+    z-index: 1;
+  }
+  .top_line {
+    position: fixed;
+    top: 0%;
+    left: 0%;
+    border-bottom: 6px solid rgb(3, 3, 3);
+    background-color: rgb(56, 55, 55);
+    width: 110vw;
+    height: 40px;
+    z-index: 1;
+  }
+</style>
---
+++ src/Login.svelte
@@ -0,0 +1,63 @@
+<script >
+import { text } from "svelte/internal";
+import { createEventDispatcher } from 'svelte';
+import {fly , fade} from'svelte/transition';
+const dispatch=createEventDispatcher();
+function close()
+{
+    dispatch('closeIt');
+}
+export let is_registered=false ;
+</script>
+
+
+
+<style>
+.background_login{
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 100vw;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 2;
+
+}
+.login_box{
+
+    position: absolute;
+    top: 550px;
+    left: 50%;
+    margin: -400px 0 0 -215px;
+    height: 250px;
+    width: 400px;
+    color: rgb(3, 3, 3);
+    padding:  15px;
+    background-color: rgb(255, 255, 255);
+    border: solid 5px rgb(5, 5, 5);
+    z-index: 100;
+    border-radius: 10px ;
+}
+</style>
+
+<div class="background_login" transition:fade on:click={close}/>
+<div class="login_box" transition:fly={{ x : -300}}>
+    <form action="">
+        <label for="username" style="position: relative; text-align: center;">Username: <input type="text"></label>
+        <label for="password" style="position: relative; text-align: center;">Password:  <input type="password"></label>
+        <button style="position: relative; left:30%;" on:click={close}>
+            {#if is_registered} 
+            Register 
+            {:else} 
+            Login 
+            {/if}</button>
+        <button style="position: relative;left:40% ;" on:click={close}>Close</button>
+        <hr>
+        <button style="position: relative;left:35%" type = "button" on:click={() => is_registered = !is_registered}>
+            {#if is_registered}
+            Logare
+            {:else}
+            Inregistrare
+            {/if}
+        </button>
+</div>
