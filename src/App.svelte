5d6434d54a3a849913f725957e0e4cb40d9d9020
Julien <julieniancu@gmail.com>
2021-02-19 15:53:37 +0200

    Added unfunctional searchbar

--- src/App.svelte
+++ src/App.svelte
@@ -1,91 +1,111 @@
 <script>
-  import Login  from "./Login.svelte";
+  import Login from "./Login.svelte";
   let is_logged = false;
   function show_register() {
-	is_registered=true;
-	show_login();
+    is_registered = true;
+    show_login();
   }
   function show_login() {
     is_logged = true;
   }
   function hide_login() {
     is_logged = false;
-	is_registered= false;
+    is_registered = false;
   }
-  let is_registered= false;
+  let is_registered = false;
 </script>
 
 <main>
-  <div class="box" />
-  <div class="top_line" />
-  <h1>Referatele grozave</h1>
-  
-  <button class="register_btn"  on:click={show_register}>Register</button>
-  <button class="login_btn" on:click={show_login}>Login</button>
-  {#if is_logged}
-    <Login {is_registered}  on:closeIt={hide_login} />		
-  {/if}
-  <img class="Carte" src="../Poze/Carti.png" alt="Cardati">
-</main>
+  <div class="banner">
+    <img class="h1" src="../Poze/Logo.jpg" alt="Logo" />
+    <img class="Carte" src="../Poze/Carti.png" alt="Cardati" />
+  </div>
+  <div class="top_line">
+    <label for="search" class="search">Cautare : <input type="search" /></label>
+    <button class="register_btn" on:click={show_register}>Register</button>
+    <button class="login_btn" on:click={show_login}>Login</button>
+    {#if is_logged}
+      <Login {is_registered} on:closeIt={hide_login} />
+    {/if}
+  </div>
+  <div class="side-bar">
+    
+  </main>
 
 <style>
-  .Carte{
-	position: absolute;
-    top: 50px;
-	left: 10%;
-	height: 140px;
-	z-index: 1;
-	
-  }
-  h1 {
-    position: absolute;
-    text-align: center;
-	left: 43%;
-	top:70px;
+  .search {
+    position:absolute;
+    color: rgb(255, 255, 255);
+    float: left;
+    margin-left: 30px;
+    top: 0;
     font-style: italic;
-    height: 30vw;
-	z-index: 1;
-    
+    height: 40px;  
+    z-index: 1;
+  }
+  .side-bar{
+    position: absolute;
+    height: 100vw;
+    width: 200px;
+    left: 0%;
+    background-color: rgb(197, 192, 192);
+    top: 236px;
+    border-right:solid 6px black;
+    z-index: 0;
+  }
+  .Carte {
+    position: fixed;
+    top: 60px;
+    left: 20%;
+    height: 140px;
+    z-index: 1;
+  }
+  .h1 {
+    position: fixed;
+    top: 60px;
+    left: 40%;
+    z-index: 1;
+    border: solid 3px rgb(0, 0, 0);
   }
   .login_btn {
-    position: absolute;
-    padding: 0px;
+    position:relative;
+    float: right;
     border: solid 3px rgb(0, 0, 0);
-    left: 92%;
-    top: 0vw;
-	font-style: italic;
+    top: 0;
+    font-style: italic;
     height: 40px;
     width: 80px;
-    z-index: 2;
+    z-index: 1;
   }
   .register_btn {
-    position: absolute;
-    padding: 0px;
+    position:relative;
     border: solid 3px rgb(0, 0, 0);
-    left: 85%;
-	font-style:italic ;
-    top: 0px;
+    float: right;
+    margin-left: 80;
+    top: 0;
+    font-style: italic;
     height: 40px;
     width: 80px;
-    z-index: 2;
+    z-index: 1;
   }
-  .box {
-    position: fixed;
-    top: 0%;
-    left: 0%;
-	background-image: url("../Poze/Fundal.jpg");
+  .banner {
+    position: absolute;
+    top: 0;
+    left: 0;
+    filter: contrast(75%) brightness(100%);
+    background-image: url("../Poze/Fundal.jpg");
     border-bottom: 6px solid rgb(3, 3, 3);
-    width: 110vw;
-    height: 200px;
+    width: 100vw;
+    height: 230px;
     z-index: 1;
   }
   .top_line {
-    position: fixed;
-    top: 0%;
-    left: 0%;
+    position: absolute;
+    top: 0;
+    left:0;
     border-bottom: 6px solid rgb(3, 3, 3);
     background-color: rgb(56, 55, 55);
-    width: 110vw;
+    width: 100vw;
     height: 40px;
     z-index: 1;
   }
--- src/Login.svelte
+++ src/Login.svelte
@@ -1,19 +1,62 @@
-<script >
-import { text } from "svelte/internal";
-import { createEventDispatcher } from 'svelte';
-import {fly , fade} from'svelte/transition';
-const dispatch=createEventDispatcher();
-function close()
-{
-    dispatch('closeIt');
-}
-export let is_registered=false ;
+<script>
+  import { text } from "svelte/internal";
+  import { createEventDispatcher } from "svelte";
+  import { fly, fade } from "svelte/transition";
+  const dispatch = createEventDispatcher();
+  function close() {
+    dispatch("closeIt");
+  }
+  export let is_registered = false;
 </script>
 
-
+<div class="background_login" transition:fade on:click={close} />
+<div class="login_box" transition:fly={{ x: -300 }}>
+  <form action="">
+    {#if !is_registered}
+      <label for="username" style="position: relative; text-align: center;"
+        >Username: <input type="text" /></label
+      >
+      <label for="password" style="position: relative; text-align: center;"
+        >Password: <input type="password" /></label
+      >
+    {:else}
+      <label for="username" style="position: relative; text-align: center;"
+        >Username: <input type="text" /></label
+      >
+      <label for="email" style="position: relative; text-align: center;"
+        >Email: <input type="email" /></label
+      >
+      <label for="password" style="position: relative; text-align: center;"
+        >Password: <input type="password" /></label
+      >
+    {/if}
+    <button style="position: relative; left:25%;" on:click={close}>
+      {#if is_registered}
+        Register
+      {:else}
+        Login
+      {/if}</button
+    >
+    <button style="position: relative;left:40% ;" on:click={close}>Close</button
+    >
+    <hr />
+    <button
+      style="position: relative;left: 50%;
+        -ms-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+        margin-top: 20px;"type="button" on:click={() => (is_registered = !is_registered)}
+    >
+      {#if is_registered}
+        Logare
+      {:else}
+        Inregistrare
+      {/if}
+    </button>
+  </form>
+</div>
 
 <style>
-.background_login{
+  .background_login {
     position: fixed;
     top: 0;
     left: 0;
@@ -21,10 +64,8 @@ export let is_registered=false ;
     width: 100vw;
     background-color: rgba(0, 0, 0, 0.5);
     z-index: 2;
-
-}
-.login_box{
-
+  }
+  .login_box {
     position: absolute;
     top: 550px;
     left: 50%;
@@ -32,32 +73,10 @@ export let is_registered=false ;
     height: 250px;
     width: 400px;
     color: rgb(3, 3, 3);
-    padding:  15px;
+    padding: 15px;
     background-color: rgb(255, 255, 255);
     border: solid 5px rgb(5, 5, 5);
     z-index: 100;
-    border-radius: 10px ;
-}
+    border-radius: 10px;
+  }
 </style>
-
-<div class="background_login" transition:fade on:click={close}/>
-<div class="login_box" transition:fly={{ x : -300}}>
-    <form action="">
-        <label for="username" style="position: relative; text-align: center;">Username: <input type="text"></label>
-        <label for="password" style="position: relative; text-align: center;">Password:  <input type="password"></label>
-        <button style="position: relative; left:30%;" on:click={close}>
-            {#if is_registered} 
-            Register 
-            {:else} 
-            Login 
-            {/if}</button>
-        <button style="position: relative;left:40% ;" on:click={close}>Close</button>
-        <hr>
-        <button style="position: relative;left:35%" type = "button" on:click={() => is_registered = !is_registered}>
-            {#if is_registered}
-            Logare
-            {:else}
-            Inregistrare
-            {/if}
-        </button>
-</div>
